033e1e1f2c2d653ae8af5900fb00d34744d67ccc
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2017-05-11 15:04:14 -0300

    Implementa a inserção de livros

--- config/express.js
+++ config/express.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function(){
 	var app = express();
 
 	app.set('view engine', 'ejs');
 	app.set('views', 'views');
+
+	app.use(bodyParser.urlencoded({extended: true}));
 
 	load('routes')
 		.then('infra')
--- routes/routes.js
+++ routes/routes.js
@@ -6,8 +6,22 @@ module.exports = function(app){
 		produtos.lista(function(errors,results){
 			res.render('produtos/lista',{lista:results});
 		});
-
 		connection.end();
+	});
 
+	app.get('/produtos/form',function(req,res){
+		res.render('produtos/form');
+	});
+
+	app.post('/produtos/salva',function(req,res){
+		var connection = app.infra.dbConnection();
+		var produtos = new app.infra.models.ProdutosDAO(connection);
+
+		var produto = req.body; 
+		console.log(produto);
+
+		produtos.salva(produto,function(errors,results){
+			res.redirect('/produtos');
+		})
 	});
 }
